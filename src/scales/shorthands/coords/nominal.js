c7ecf24231541a34430900b3076f35b02a9f58c9
Luuc van der Zee <georithmics@gmail.com>
2018-12-11 16:56:35 +0800

    feat: added nominal 'equidistant' scale

--- src/scales/shorthands/coords/nominal.js
+++ src/scales/shorthands/coords/nominal.js
@@ -1,1 +1,22 @@
-// TODO
+export default {
+  equidistant
+}
+
+// TODO better name
+function equidistant (domain, range, padStart = 0, padEnd = 0) {
+  let deltaRange = range[1] - range[0]
+  let paddedRange = deltaRange - padStart - padEnd
+
+  let sections = domain.size
+  let sectionLength = paddedRange / sections
+
+  let mapping = {}
+  let i = 0
+
+  domain.forEach(entry => {
+    mapping[entry] = range[0] + padStart + (i * sectionLength) + (sectionLength / 2)
+    i++
+  })
+
+  return x => mapping[x]
+}
