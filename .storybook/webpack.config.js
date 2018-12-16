2f3879717fe13c645f8ac20a91be60162287376d
Ate Poorthuis <atepoorthuis@gmail.com>
2018-12-16 17:52:23 +0800

    feat: story source addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,12 @@ function resolve (dir) {
 }
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  // storybook storysource addon
+  defaultConfig.module.rules.push({
+    test: /index.js/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre'
+  })
   // Extend defaultConfig to alias @ to src/
   defaultConfig.resolve.alias['@'] = resolve('src')
   return defaultConfig
