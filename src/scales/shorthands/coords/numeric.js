a961d4bebf9543dd0b2323d31ea8cf961147de89
Ate Poorthuis <atepoorthuis@gmail.com>
2018-12-24 11:38:51 +0800

    fix: remove unnecessary domainCopy in square scale

--- src/scales/shorthands/coords/numeric.js
+++ src/scales/shorthands/coords/numeric.js
@@ -24,23 +24,11 @@ function log (domain, range) {
 }
 
 function square (domain, range) {
-  let domainCopy = JSON.parse(JSON.stringify(domain))
-  if (domainCopy[0] === 0) {
-    domainCopy[0] += 1e-6
-  }
-
-  let scale = d3.scalePow().exponent(2).domain(domainCopy).range(range)
-
+  let scale = d3.scalePow().exponent(2).domain(domain).range(range)
   return scale
 }
 
 function squareRoot (domain, range) {
-  let domainCopy = JSON.parse(JSON.stringify(domain))
-  if (domainCopy[0] === 0) {
-    domainCopy[0] += 1e-6
-  }
-
   let scale = d3.scalePow().exponent(1 / 2).domain(domain).range(range)
-
   return scale
 }
