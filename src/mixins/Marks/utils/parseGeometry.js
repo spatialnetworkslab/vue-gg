4dad0e21c7c99e2887299b783ef7bad5f65cdb24
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2018-12-27 10:29:13 +0800

    feat: non-quantitative geometries for polygons

--- src/mixins/Marks/utils/parseGeometry.js
+++ src/mixins/Marks/utils/parseGeometry.js
@@ -5,7 +5,7 @@ import { invalidValueForRangeType } from './parseCoordinateSet.js'
 export default function (prop) {
   if (!this.$$map) {
     if (is(prop) && prop.constructor === Array) {
-      return prop
+      return parseArray(prop, this.parentRangeTypes, this.parentBranch)
     }
 
     if (isnt(prop)) {
@@ -15,7 +15,7 @@ export default function (prop) {
 
   if (this.$$map) {
     if (is(prop) && prop.constructor === Array) {
-      return { assign: prop }
+      return { assign: parseArray(prop, this.parentRangeTypes, this.parentBranch) }
     }
 
     if (isnt(prop)) {
@@ -24,22 +24,32 @@ export default function (prop) {
   }
 }
 
-function parseArray (data, parentRangeType, dimension, parentBranch) {
-  // TODO nested
+function parseArray (data, parentRangeTypes, parentBranch) {
   // Here we check whether array entries' types (String, Number, etc)
   // are compatible with the parent domain (categorical, quantitative, etc)
+  let types = ['x', 'y']
+
   let parsed = []
   for (let entry of data) {
-    if (invalidValueForRangeType(entry, parentRangeType)) {
-      throw new Error(`Invalid input ${entry} for parent Section domain type ${parentRangeType}`)
-    } else {
-      if (['categorical', 'temporal'].includes(parentRangeType)) {
-        // We will already convert categorical and temporal data here.
-        parsed.push(convertToNumeric(entry, dimension, parentBranch))
+    let point = []
+
+    for (let i = 0; i < types.length; ++i) {
+      let type = types[i]
+      let parentRangeType = parentRangeTypes[type]
+
+      if (invalidValueForRangeType(entry[i], parentRangeType)) {
+        throw new Error(`Invalid input ${entry[i]} for parent Section domain type ${parentRangeType}`)
       } else {
-        parsed.push(entry)
+        if (['categorical', 'temporal'].includes(parentRangeType)) {
+          // We will already convert categorical and temporal data here.
+          point.push(convertToNumeric(entry[i], type, parentBranch))
+        } else {
+          point.push(entry[i])
+        }
       }
     }
+
+    parsed.push(point)
   }
 
   return parsed
