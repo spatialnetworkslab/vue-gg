70572ff971d562423ac4d622a5fbc94033cfdfde
gracegsy <graceguo43@gmail.com>
2019-01-16 14:07:14 +0800

    feat: more classification methods

--- src/transformations/transformations/binning.js
+++ src/transformations/transformations/binning.js
@@ -1,49 +1,60 @@
+import * as Geostats from 'geostats'
+import dataLength from '../utils/dataLength.js'
+
 export default function (data, binningObj) {
   if (binningObj.constructor !== Object) {
-    throw new Error('Binning only accepts an object')
+    throw new Error('Binning only accepts an Object')
   }
 
-  if (this.$$dataContainer) { console.log(this.$$dataContainer) }
-
-  for (let key in binningObj) {
-    if (data.hasOwnProperty(key)) {
-      let binCount = binningObj[key].binCount
-      let binSize = binningObj[key].binSize
-      let binInterval = binningObj[key].binInterval
-
-      let variableData = data[key]
-
-      if (!(binCount ^ binSize ^ binInterval)) {
-        throw new Error('Please provide only one of the following: bin count, bin size, custom bin ranges')
-      } else if (binCount) {
-        let domain = variableDomain(variableData)
-        let interval = (domain[1] - domain[0]) / binCount
-
-        let ranges = rangeFromInterval(domain, interval, binCount)
-
-        let newData = bin(variableData, ranges)
-        console.log(newData)
-
-        return newData
-      } else if (binSize) {
-        let domain = variableDomain(variableData)
-        let binCount = Math.round((domain[1] - domain[0]) / binSize)
-
-        let ranges = rangeFromInterval(domain, binSize, binCount)
-
-        let newData = bin(variableData, ranges)
-        return newData
-      } else {
-        let newData = bin(variableData, binInterval)
-
-        return newData
-      }
-    } else {
-      console.warn(`Column '${key}' not found`)
-    }
+  let key = binningObj.groupBy
+  if (key.constructor !== String) {
+    throw new Error('groupBy only accepts a String variable name')
   }
+
+  let method = binningObj.method
+  if (method.constructor !== String) {
+    throw new Error('Please provide the classification method you will like to use')
+  }
+
+  let numClasses = binningObj.numClasses
+
+  let variableData = data[key]
+  let geoStat = new Geostats(variableData)
+
+  let ranges
+
+  // Calculate ranges to obtain bins of a specified size
+  if (method === 'IntervalSize') {
+    let binSize = binningObj.binSize
+    let domain = variableDomain(variableData)
+    let binCount = Math.round((domain[1] - domain[0]) / binSize)
+
+    ranges = rangeFromInterval(domain, binSize, binCount)
+  } else if (method === 'EqualInterval') {
+    ranges = geoStat.getClassEqInterval(numClasses)
+  } else if (method === 'StandardDeviation') {
+    ranges = geoStat.getClassStdDeviation(numClasses)
+  } else if (method === 'ArithmeticProgression') {
+    ranges = geoStat.getClassArithmeticProgression(numClasses)
+  } else if (method === 'GeometricProgression') {
+    ranges = geoStat.getClassGeometricProgression(numClasses)
+  } else if (method === 'Quantile') {
+    ranges = geoStat.getClassQuantile(numClasses)
+  } else if (method === 'Jenks') {
+    ranges = geoStat.getClassJenks(numClasses)
+  } else if (method === 'UniqueValues') {
+    ranges = geoStat.getClassUniqueValues()
+  } else if (method === 'Manual') {
+    ranges = binningObj.manualClasses
+  }
+
+  ranges = pairRange(ranges)
+
+  let newData = bin(data, key, ranges)
+  return newData 
 }
 
+// Extract domain of variable of interest
 function variableDomain (column) {
   let asc = column.sort((a, b) => a - b)
 
@@ -56,6 +67,8 @@ function variableDomain (column) {
 
 function rangeFromInterval (domain, interval, binCount) {
   let ranges = []
+
+  // Ranges should start at the minimum value of variable of interest
   let lowerBound = domain[0]
 
   for (let i = 0; i < binCount; i++) {
@@ -69,38 +82,67 @@ function rangeFromInterval (domain, interval, binCount) {
   return ranges
 }
 
-function binIndexFromValue (value, lowerBound, binInterval, upperBound) {
-  if (value >= upperBound) {
-    value -= 1
+function pairRange(ranges) {
+  let l = ranges.length
+
+  let newRange = []
+
+  for (let i = 0; i < l - 1; i ++) {
+    newRange.push([ranges[i], ranges[i+1]])
   }
-  return Math.floor((value - lowerBound) / binInterval)
+
+  return newRange
 }
 
-function bin (data, ranges) {
+function bin (data, variable, ranges) {
   let newData = {}
 
-  newData.min = []
-  newData.max = []
-  newData.n = Array(ranges.length).fill(0)
-  newData.sum = Array(ranges.length).fill(0)
-  newData.average = []
+  newData.lowerBound = []
+  newData.upperBound = []
 
-  for (let ix = 0; ix < data.length; ix++) {
-    let instance = data[ix]
-    let binIndex = ranges.findIndex(el => instance >= el[0] && instance <= el[1])
-    console.log(binIndex)
-    newData.n[binIndex]++
-    newData.sum[binIndex] = newData.sum[binIndex] + instance
-  }
-
+  // Create lowerBound and upperBound columns to track bin ranges
   for (let i = 0; i < ranges.length; i++) {
-    newData.min.push(ranges[i][0])
-    newData.max.push(ranges[i][1])
-
-    let a = newData.sum[i] / newData.n[i]
-
-    newData.average.push(a)
+    newData.lowerBound.push(ranges[i][0])
+    newData.upperBound.push(ranges[i][1])
   }
+
+  let ix = 0
+
+  // Create an empty array to store new dataFrames divided by range
+  let bins = Array(ranges.length)
+
+  let length = dataLength(data)
+
+  // Loop through data
+  while (ix < length) {
+    let instance = data[variable][ix]
+
+    // Find index of bin in which the instance belongs
+    let binIndex = ranges.findIndex(el => instance >= el[0] && instance <= el[1])
+
+    let newRow = bins[binIndex]
+
+    // If dataFrame does not exist, create it
+    if (!newRow) {newRow = {}}
+
+    for (let col in data) {
+      // If data key does not exist, create it
+      if (!newRow[col]) {
+        newRow[col] = [data[col][ix]]
+      } else {
+        newRow[col].push(data[col][ix])
+      }
+    }
+
+    // Update the bins column with new dataFrame
+    bins[binIndex] = newRow
+
+    ix++
+  }
+
+  // Add new dataFrame column to newData
+  newData.grouped = bins
 
   return newData
 }
+
