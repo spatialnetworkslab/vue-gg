a5dd2f2b0c62c001e2229194ada943a33811db39
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-02-11 18:01:43 +0800

    feat: working on repeat component

--- src/components/Core/Repeat.vue
+++ src/components/Core/Repeat.vue
@@ -1,7 +1,51 @@
 <script>
 export default {
   props: {
-    
+    x: {
+      type: [Array, undefined],
+      default: undefined
+    },
+
+    y: {
+      type: [Array, undefined],
+      default: undefined
+    }
+  },
+
+  methods: {
+    validateSlotContent (slotContent) {
+      let definedElements = slotContent.filter(c => c.tag !== undefined)
+      if (definedElements.some(c => c.componentOptions.tag !== 'vgg-section')) {
+        throw new Error(`vgg-repeat can only contain sections`)
+      }
+    },
+
+    validateProps () {
+      if (this.x === undefined && this.y === undefined) {
+        return false
+      }
+
+      if (this.x) {
+
+      }
+
+      if (this.y) {
+        
+      }
+
+      return true
+    }
+  },
+
+  render (createElement) {
+    let slotContent = this.$slots.default
+    this.validateSlotContent(slotContent)
+
+    let allowed = this.validateProps()
+
+    if (allowed) {
+      return createElement()
+    }
   }
 }
 </script>
