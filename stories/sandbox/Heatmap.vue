6f3330f39562459aa966c4940ed0dc06d32c9d48
Bianchi <bianchi_dy@sutd.edu.sg>
2019-02-20 15:43:13 +0800

    refactor: implement 2, 5, 10 dimension heatmaps

--- stories/sandbox/Heatmap.vue
+++ stories/sandbox/Heatmap.vue
@@ -11,24 +11,25 @@
         :text="title"
         :hjust="'center'"/>
 
+
       <vgg-section
         :x1="baseX"
         :x2="sectionWidth + baseX"
-        :y1="baseY"
-        :y2="sectionHeight + baseY"
+        :y1="baseY + sectionHeight * 2"
+        :y2="sectionHeight * 2.2 + baseY"
         :scale-x="[baseX, sectionWidth + baseX]"
         :scale-y="[baseY, sectionHeight + baseY]"
       >
-        <g v-for="(item, i) in segments">
-          <vgg-data v-if="segments" :data="item">
 
+        <g v-for="(item, i) in segments(2, 40)">
+          <vgg-data v-if="segments" :data="item">
             <vgg-map v-slot="{ row }">
               <vgg-rectangle
               :x1="row.x1"
               :x2="row.x2"
               :y1="row.y1"
               :y2="row.y2"
-              :fill="{val: row.value, scale: { type: 'blues', domain: 'value'}}"
+              :fill="{val: row.value, scale: { type: 'purples', domain: 'value'}}"
               />
             </vgg-map>
           </vgg-data>
@@ -44,7 +45,93 @@
       />
 
       <vgg-y-axis
-        :scale="categories"
+        :scale="categories2"
+        title="Attributes"
+        :hjust="-0.02"
+        :vjust="0.3"
+        :titleVjust="1.2"
+        flip
+      />
+
+      </vgg-section>
+
+      <vgg-section
+        :x1="baseX"
+        :x2="sectionWidth + baseX"
+        :y1="baseY + sectionHeight * 1.3"
+        :y2="sectionHeight * 1.7 + baseY"
+        :scale-x="[baseX, sectionWidth + baseX]"
+        :scale-y="[baseY, sectionHeight + baseY]"
+      >
+
+        <g v-for="(item, i) in segments(5, 40)">
+          <vgg-data v-if="segments" :data="item">
+            <vgg-map v-slot="{ row }">
+              <vgg-rectangle
+              :x1="row.x1"
+              :x2="row.x2"
+              :y1="row.y1"
+              :y2="row.y2"
+              :fill="{val: row.value, scale: { type: 'purples', domain: 'value'}}"
+              />
+            </vgg-map>
+          </vgg-data>
+        </g>
+
+      <vgg-x-axis
+        :scale="'Name'"
+        title="Drinks"
+        :titleHjust="1.03"
+        :vjust="-.05"
+        :labelFontSize="8"
+        labelRotate
+      />
+
+      <vgg-y-axis
+        :scale="categories5"
+        title="Attributes"
+        :hjust="-0.02"
+        :vjust="0.3"
+        :titleVjust="1.1"
+        flip
+      />
+
+      </vgg-section>
+
+      <vgg-section
+        :x1="baseX"
+        :x2="sectionWidth + baseX"
+        :y1="baseY"
+        :y2="sectionHeight + baseY"
+        :scale-x="[baseX, sectionWidth + baseX]"
+        :scale-y="[baseY, sectionHeight + baseY]"
+      >
+
+        <g v-for="(item, i) in segments(10, 40)">
+          <vgg-data v-if="segments" :data="item">
+            <vgg-map v-slot="{ row }">
+              <vgg-rectangle
+              :x1="row.x1"
+              :x2="row.x2"
+              :y1="row.y1"
+              :y2="row.y2"
+              :fill="{val: row.value, scale: { type: 'purples', domain: 'value'}}"
+              />
+            </vgg-map>
+          </vgg-data>
+        </g>
+
+      <vgg-x-axis
+        :scale="'Name'"
+        title="Drinks"
+        :titleHjust="1.03"
+        :vjust="-.05"
+        :labelFontSize="8"
+        labelRotate
+      />
+
+      <vgg-y-axis
+        :scale="categories10"
         title="Attributes"
         :hjust="-0.02"
         flip
@@ -61,19 +148,22 @@
 import {csv} from 'd3-fetch'
 
 export default {
-  name: 'idc',
+  name: 'idc-heatmap',
 
   data () {
     return {
       data: undefined,
       title: "Drinks Heatmap",
-      categories: ['Calories', 'Protein', 'Sugars', 'Carbohydrates', 'SaturatedFat', 'TransFat', 'Cholesterol', 'Sodium', 'Fibre', 'VitaminA', 'VitaminC', 'Calcium', 'Iron'],
+      categories2: ['Sugars', 'Calories'],
+      categories5: ['Calories', 'Protein', 'Sugars', 'Carbohydrates', 'Cholesterol'],
+      categories10: ['Calories', 'Protein', 'Sugars', 'Carbohydrates', 'SaturatedFat', 'TransFat', 'Cholesterol', 'Sodium', 'Fibre', 'VitaminA', 'VitaminC', 'Calcium', 'Iron'],
       width: 1000,
-      height: 900,
-      sectionWidth: 850,
+      height: 1000,
+      sectionWidth: 800,
       sectionHeight: 300,
       baseX: 100,
-      baseY: 500
+      baseY: 200,
+
     }
   },
 
@@ -81,36 +171,49 @@ export default {
     this.drinks()
   },
 
-  computed: {
-    segments(){
-      console.log(this.data)
+  methods: {
+    segments(dimensions, options){
       if (this.data) {
+        let categories = this.categories10
+
+
+        if (!isNaN(dimensions)){
+          if (dimensions === 2) {
+            categories = this.categories2
+          } else if (dimensions === 5) {
+            categories = this.categories5
+          } else if (dimensions === 10) {
+            categories = this.categories10
+          }
+        }
+
+        if (isNaN(options)){
+          let options = this.data.length
+        }
+
         let segments = []
         let widthDelta = this.sectionWidth/this.data.length
-        console.log(this.data)
-        let heightDelta = this.sectionHeight/this.categories.length
+        let heightDelta = this.sectionHeight/categories.length
         let x = this.baseX, y = this.baseY
 
-        for (let i = 0; i < this.categories.length; i++) {
+        for (let i = 0; i < categories.length; i++) {
             segments[i] = []
-            for (let j = 0; j < this.data.length; j++) {
+            for (let j = 0; j < options; j++) {
               let macro = {}
               macro.x1 = x + widthDelta * j
               macro.x2 = x + widthDelta * (j+1)
               macro.y1 = y + heightDelta * i
               macro.y2 = y + heightDelta * (i+1)
-              macro.value = this.data[j][this.categories[i]]
-              macro.attribute = this.categories[i]
+              macro.value = this.data[j][categories[i]]
+              macro.attribute = categories[i]
               macro.name = this.data[j].Name
               segments[i].push(macro)
             }
         }
         return segments
       }
-    }
-  },
+    },
 
-  methods: {
     drinks () {
       // change name of csv
       csv('../../static/idcDemoDrinksDaily.csv').then((data) => {
