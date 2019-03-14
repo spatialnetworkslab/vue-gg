f35720f9f89caa5779ebb58cc6dc1b9981c1df4d
Bianchi <bianchi_dy@sutd.edu.sg>
2019-03-14 14:49:00 +0800

    chore: add console warn messages for mismatch in length of domain and range arrays for color and shape scales

--- src/scales/shorthands/shape/scaleFromRange.js
+++ src/scales/shorthands/shape/scaleFromRange.js
@@ -2,6 +2,12 @@ export default function (domain, ranges) {
   let mapping = {}
   let i = 0
 
+  if (domain.length > ranges.length) {
+    console.warn('Length of domain array is greater than length of ranges array. Scaling will wrap around range given.')
+  } else if (domain.length < ranges.length) {
+    console.warn('Length of domain array is smaller than length of ranges array. Scaling will only be until end of domain given.')
+  }
+  
   domain.forEach(entry => {
     mapping[entry] = ranges[i % ranges.length]
     i++
