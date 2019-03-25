22b767a94bb0bf0261c3d8e4edea7f2c523884f8
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-03-25 16:40:37 +0100

    fix: categorical scale domain order

--- src/scales/utils/parseScaleOptions.js
+++ src/scales/utils/parseScaleOptions.js
@@ -122,6 +122,17 @@ function updateDomain (domain, domainType, scalingOptions, dataInterface) {
       }
     }
 
+    if (is(scalingOptions.order)) {
+      let sortedDomain = [...scalingOptions.order]
+      for (let cat of newDomain) {
+        if (!sortedDomain.includes(cat)) {
+          sortedDomain.unshift(cat)
+        }
+      }
+
+      newDomain = sortedDomain
+    }
+
     return newDomain
   } else { return domain }
 }
@@ -131,8 +142,14 @@ function validScalingOptions (domainType, scalingOptions) {
     if (hasAnyWrongProperty(scalingOptions)) {
       throw new Error(`Invalid scaling options for categorical domain: ${JSON.stringify(scalingOptions)}`)
     }
+    if (scalingOptions.order && scalingOptions.order.constructor !== Array) {
+      throw new Error(`'order' must be array`)
+    }
     return true
   } else {
+    if (scalingOptions.hasOwnProperty('order')) {
+      throw new Error(`Invalid scaling option: 'order'. Can only be used with categorical domain`)
+    }
     checkTypes(domainType, scalingOptions)
     return true
   }
