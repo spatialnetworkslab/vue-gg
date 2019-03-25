fd31f85a1df31fbf9c6655713c8e492939388747
luucvanderzee <luuc_v_d_zee@hotmail.com>
2019-03-25 15:47:37 +0100

    fix: global scale bug (-> facets)
    * fix: global scale bug
    
    * fix: tried to fix bug where code should stop after error but doesn't (failed)
    
    * _
    
    * fix: not much, trying to make Ate's facet example but running into other bugs
    
    * fix: y-axis bug
    
    * rename: Facets

--- stories/index.js
+++ stories/index.js
@@ -27,6 +27,8 @@ import SectionAxesTest from './sandbox/SectionAxesTest.vue'
 import SelectTest from './sandbox/SelectTest.vue'
 import MultipleSelections from './sandbox/MultipleSelections.vue'
 import NestedSelection from './sandbox/NestedSelection.vue'
+import AteAxisScaleBug from './sandbox/AteAxisScaleBug.vue'
+import Facets from './sandbox/Facets.vue'
 
 storiesOf('Charts', module)
   .add('Scatterplot', () => (Scatterplot))
@@ -57,3 +59,5 @@ storiesOf('Sandbox', module)
   .add('Selection test', () => (SelectTest))
   .add('Multiple selections', () => (MultipleSelections))
   .add('Nested selection', () => (NestedSelection))
+  .add('Ate axis scale bug', () => (AteAxisScaleBug))
+  .add('Facets', () => (Facets))
---
+++ stories/sandbox/Facets.vue
@@ -0,0 +1,88 @@
+<template>
+  <vgg-graphic
+    :width="1000"
+    :height="800"
+    :data="resaleData"
+  >
+
+    <vgg-scales :scales="{ typeScale: 'flat_type' }" />
+
+    <vgg-data
+      :transform="[
+        { groupBy: ['town', 'flat_type'] },
+        { summarise: { total_sales: { resale_price: 'count' } } }
+      ]"
+    >
+
+      <vgg-scales :scales="{ totalScale: { domain: 'total_sales', domainMin: 0 } }" />
+
+    </vgg-data>
+
+    <vgg-data :transform="{ groupBy: 'town' }">
+
+      <vgg-grid
+        :rows="4"
+        :layout-padding="15"
+        :cell-padding="10"
+      >
+
+        <vgg-map v-slot="{ row: facet }">
+
+          <vgg-section
+            :data="facet.grouped"
+            :transform="[
+              { groupBy: 'flat_type'},
+              { summarise: { total_sales: { resale_price: 'count' } } }
+            ]"
+          >
+
+            <vgg-map v-slot="{ row }">
+
+              <vgg-rectangle
+                :y="{ val: row.flat_type, scale: '#typeScale' }"
+                :x1="{ val: 0, scale: '#totalScale' }"
+                :x2="{ val: row.total_sales, scale: '#totalScale' }"
+                :h="20"
+              />
+
+            </vgg-map>
+
+            <vgg-y-axis :scale="'#typeScale'" />
+            <vgg-x-axis
+              :scale="'#totalScale'"
+              :tick-count="4"
+              rotate-label
+            />
+
+          </vgg-section>
+
+        </vgg-map>
+
+      </vgg-grid>
+
+    </vgg-data>
+
+  </vgg-graphic>
+</template>
+
+<script>
+import resaleData from './resale_sample.json'
+
+export default {
+  data () {
+    return {
+      resaleData: resaleData
+    }
+  }
+  // mounted () {
+  //   let unique = {}
+  //   for (let row of resaleData) {
+  //     unique[row.town] = true
+  //   }
+  //   console.log(Object.keys(unique))
+  // }
+}
+</script>
+
+<style>
+</style>
