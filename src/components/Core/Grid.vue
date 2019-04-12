spatialnetworkslab/vue-gg

commit 138c2914cec47e50fd9d3857963abda827771a20

feat: Grid accept custom components (#106)
* feat: vgg-grid now support 'square' components

* docs: added 'square' component to docs

* feat: square components also accepted in vgg-repeat

src/components/Core/Grid.vue
@@ -3,8 +3,9 @@ import {
   calculateGridLayout,
   calculateRowsCols,
   validateGridOptions,
-  updateGridSections
+  updateGridComponents
 } from './utils/grid.js'
+import isSquareComponent from './utils/isSquareComponent.js'
 import CoordinateTreeUser from '../../mixins/CoordinateTreeUser.js'
 import DataReceiver from '../../mixins/Data/DataReceiver.js'
 
@@ -43,8 +44,8 @@ export default {
       let definedChildren = children.filter(c => c.tag !== undefined)
 
       let wrongUseError = new Error(`'vgg-grid' can have in its slot:
-        1. any number of 'vgg-section' components
-        2. a single 'vgg-map' component that only contains other 'vgg-section' components
+        1. any number of 'square' (with x1, x2, y1 and y2 props) components
+        2. a single 'vgg-map' component that only contains other 'square' components
       `)
 
       if (definedChildren[0].componentOptions.tag === 'vgg-map') {
@@ -54,12 +55,16 @@ export default {
         return [definedChildren, 'map']
       }
 
-      if (definedChildren.some(c => c.componentOptions.tag !== 'vgg-section')) {
+      if (definedChildren.some(c => !this.isSquareComponent(c))) {
         throw wrongUseError
       } else {
-        return [definedChildren, 'section']
+        return [definedChildren, 'square']
       }
     }
+  },
+
+  methods: {
+    isSquareComponent
   },
 
   provide () {
@@ -76,18 +81,18 @@ export default {
 
     let [children, childType] = this.children
 
-    if (childType === 'section') {
-      let sections = children
+    if (childType === 'square') {
+      let squareComponents = children
 
-      let numberOfSections = sections.length
-      let { rows, cols } = calculateRowsCols(options, numberOfSections)
+      let numberOfSquareComponents = squareComponents.length
+      let { rows, cols } = calculateRowsCols(options, numberOfSquareComponents)
       let ranges = this.parentBranch.domains
       let start = this.start
       let layout = calculateGridLayout(rows, cols, options, ranges, undefined, start)
 
-      let newSections = updateGridSections(createElement, sections, layout)
+      let newComponents = updateGridComponents(squareComponents, layout)
 
-      return createElement('g', { class: 'layout-grid' }, newSections)
+      return createElement('g', { class: 'layout-grid' }, newComponents)
     }
 
     if (childType === 'map') {

src/components/Core/utils/grid.js
@@ -1,5 +1,3 @@
-import Section from '../Section.vue'
-
 export function validateGridOptions (options) {
   let hasRows = options.rows !== undefined
   let hasCols = options.cols !== undefined
@@ -74,15 +72,15 @@ export function calculateGridLayout (rows, cols, options, ranges, axes, start) {
   return cells
 }
 
-export function updateGridSections (createElement, sections, gridLayout) {
-  let newSections = []
-  for (let i = 0; i < sections.length; i++) {
-    let section = sections[i]
+export function updateGridComponents (components, gridLayout) {
+  let newComponents = []
+  for (let i = 0; i < components.length; i++) {
+    let component = components[i]
     let layout = gridLayout[i]
-    newSections.push(updateSection(createElement, section, layout))
+    newComponents.push(updateComponent(component, layout))
   }
 
-  return newSections
+  return newComponents
 }
 
 function getPadding (padding) {
@@ -106,11 +104,11 @@ function getPadding (padding) {
   return paddings
 }
 
-export function updateSection (createElement, section, layout) {
-  let props = mergeProps(layout, section.componentOptions.propsData)
-  let slots = section.componentOptions.children
-  let newSection = createElement(Section, { props }, slots)
-  return newSection
+export function updateComponent (component, layout) {
+  let clonedComponent = component
+  let props = mergeProps(layout, component.componentOptions.propsData)
+  clonedComponent.componentOptions.propsData = props
+  return clonedComponent
 }
 
 function mergeProps (coords, other) {

stories/sandbox/SimpleGridLayout.vue
@@ -10,7 +10,7 @@
       :cell-padding="{ l: 7, r: 20, b: 4 }"
     >
 
-      <vgg-section
+      <!-- <vgg-section
         v-for="s in range(5)"
         :key="s"
         :scale-x="[0, 1]"
@@ -25,7 +25,12 @@
           :fill="randomColor()"
         />
 
-      </vgg-section>
+      </vgg-section> -->
+      <custom-square-component
+        v-for="s in range(5)"
+        :key="s"
+        :fill="randomColor()"
+      />
 
     </vgg-grid>
 
@@ -33,7 +38,11 @@
 </template>
 
 <script>
+import CustomSquareComponent from './CustomSquareComponent.vue'
+
 export default {
+  components: { CustomSquareComponent },
+
   methods: {
     range (n) {
       return new Array(n).fill(0).map((v, i) => i)

src/components/Core/Map.vue
@@ -2,11 +2,12 @@
 import DataReceiver from '../../mixins/Data/DataReceiver.js'
 import CoordinateTreeUser from '../../mixins/CoordinateTreeUser.js'
 import ScaleReceiver from '../../mixins/Scales/ScaleReceiver.js'
+import isSquareComponent from './utils/isSquareComponent.js'
 
 import {
   calculateGridLayout,
   calculateRowsCols,
-  updateGridSections
+  updateGridComponents
 } from './utils/grid.js'
 
 import { initMappingTree, extractMappings, mapRow } from './utils/mappings.js'
@@ -70,16 +71,21 @@ export default {
       return mappedElements
     },
 
-    validateSections (elements) {
+    validateComponents (elements) {
+      let wrongUseError = new Error(`'vgg-grid' can have in its slot:
+        1. any number of 'square' (with x1, x2, y1 and y2 props) components
+        2. a single 'vgg-map' component that only contains other 'square' components
+      `)
+
       elements = elements.filter(el => el.tag !== undefined)
-      if (!elements.every(el => el.componentOptions.tag === 'vgg-section')) {
-        throw new Error(`'vgg-grid' can have in its slot:
-          1. any number of 'vgg-section' components
-          2. a single 'vgg-map' component that only contains other 'vgg-section' components
-        `)
+      if (elements.some(c => !this.isSquareComponent(c))) {
+        throw wrongUseError
+      } else {
+        return elements
       }
-      return elements
-    }
+    },
+
+    isSquareComponent
   },
 
   provide () {
@@ -102,16 +108,16 @@ export default {
     })
 
     if (this.$$grid !== false) {
-      let sections = this.validateSections(elements)
+      let squareComponents = this.validateComponents(elements)
       let options = this.$$grid
 
-      let numberOfSections = sections.length
-      let { rows, cols } = calculateRowsCols(options, numberOfSections)
+      let numberOfSquareComponents = squareComponents.length
+      let { rows, cols } = calculateRowsCols(options, numberOfSquareComponents)
       let ranges = this.parentBranch.domains
       let start = this.$$grid.start
       let layout = calculateGridLayout(rows, cols, options, ranges, undefined, start)
 
-      elements = updateGridSections(createElement, sections, layout)
+      elements = updateGridComponents(squareComponents, layout)
     }
 
     return createElement('g', { class: 'map' }, elements)

src/components/Core/Repeat.vue
@@ -1,6 +1,6 @@
 <script>
 import { calculateGridLayout } from './utils/grid.js'
-import { calculateRowsCols, repeatSections } from './utils/repeat.js'
+import { calculateRowsCols, repeatComponents } from './utils/repeat.js'
 import CoordinateTreeUser from '../../mixins/CoordinateTreeUser.js'
 
 export default {
@@ -56,7 +56,7 @@ export default {
     let sides = this.sides
 
     let slot = this.$scopedSlots.default
-    let newSections = repeatSections(createElement, slot, layout, this.x, this.y, sides)
+    let newSections = repeatComponents(slot, layout, this.x, this.y, sides)
 
     return createElement('g', { class: 'layout-repeat' }, newSections)
   }

src/components/Core/utils/isSquareComponent.js
@@ -1,0 +1,10 @@
+export default function (component) {
+  let props = component.componentOptions.Ctor.options.props
+  let squareProps = ['x1', 'x2', 'y1', 'y2']
+  for (let prop of squareProps) {
+    if (!props[prop]) {
+      return false
+    }
+  }
+  return true
+}

src/components/Core/utils/repeat.js
@@ -1,7 +1,8 @@
-import { updateSection } from './grid.js'
+import { updateComponent } from './grid.js'
+import isSquareComponent from './isSquareComponent.js'
 
-export function repeatSections (createElement, slot, layout, xValues, yValues, sides) {
-  let newSections = []
+export function repeatComponents (slot, layout, xValues, yValues, sides) {
+  let newComponents = []
 
   let rowLength = yValues.length
   let colLength = xValues.length
@@ -12,26 +13,26 @@ export function repeatSections (createElement, slot, layout, xValues, yValues, s
       let x = xValues[colIndex]
 
       let slotContent = slot({ x, y })
-      let sections = validateSlotContent(slotContent)
+      let components = validateSlotContent(slotContent)
 
       let layoutIndex = (rowIndex * colLength) + colIndex
       let currentLayout = layout[layoutIndex]
 
-      let updatedSections = sections.map(section => {
-        return updateSection(createElement, section, currentLayout)
+      let updatedComponents = components.map(section => {
+        return updateComponent(section, currentLayout)
       })
 
       let currentSides = getSides(rowIndex, colIndex, rowLength, colLength)
 
-      updatedSections = updatedSections.map(section => {
+      updatedComponents = updatedComponents.map(section => {
         return hideSectionAxes(section, currentSides, sides)
       })
 
-      newSections.push(...updatedSections)
+      newComponents.push(...updatedComponents)
     }
   }
 
-  return newSections
+  return newComponents
 }
 
 export function calculateRowsCols (x, y) {
@@ -52,8 +53,8 @@ export function createAxesProps (axis, axisOptions, range, repeatVals) {
 
 function validateSlotContent (slotContent) {
   let definedElements = slotContent.filter(c => c.tag !== undefined)
-  if (definedElements.some(c => c.componentOptions.tag !== 'vgg-section')) {
-    throw new Error(`vgg-repeat can only contain sections`)
+  if (definedElements.some(c => !isSquareComponent(c))) {
+    throw new Error(`vgg-repeat can only contain 'square' components`)
   }
   return definedElements
 }

stories/sandbox/CustomSquareComponent.vue
@@ -1,0 +1,53 @@
+<template>
+  <vgg-section
+    :x1="x1"
+    :x2="x2"
+    :y1="y1"
+    :y2="y2"
+    :scale-x="[0, 1]"
+    :scale-y="[0, 1]"
+  >
+
+    <vgg-point
+      :x="0.5"
+      :y="0.5"
+    />
+
+    <vgg-rectangle
+      :x1="0"
+      :x2="1"
+      :y1="0"
+      :y2="1"
+      :fill="fill"
+    />
+
+  </vgg-section>
+
+</template>
+
+<script>
+export default {
+  props: {
+    x1: {
+      type: Number,
+      required: true
+    },
+    x2: {
+      type: Number,
+      required: true
+    },
+    y1: {
+      type: Number,
+      required: true
+    },
+    y2: {
+      type: Number,
+      required: true
+    },
+    fill: {
+      type: String,
+      default: 'black'
+    }
+  }
+}
+</script>

commit 6eeb767addecc06a9ef7ffea4559de2a7910a0e1

fix: grid reactivity broken after #106 (#120)
* fix: grid not throwing error if without children

* fix: vgg-grid reactive again!

* fix: replace testing code by real code

* fix: last edge case

src/components/Core/Grid.vue
@@ -37,11 +37,19 @@ export default {
   },
 
   computed: {
-    children () {
+    childType () {
       let children = this.$slots.default
+
+      if (!children) {
+        return undefined
+      }
 
       // Filter out undefined components (whitespace, v-if="false")
       let definedChildren = children.filter(c => c.tag !== undefined)
+
+      if (definedChildren.length === 0) {
+        return undefined
+      }
 
       let wrongUseError = new Error(`'vgg-grid' can have in its slot:
         1. any number of 'square' (with x1, x2, y1 and y2 props) components
@@ -52,13 +60,13 @@ export default {
         if (definedChildren.length > 1) {
           throw wrongUseError
         }
-        return [definedChildren, 'map']
+        return 'map'
       }
 
       if (definedChildren.some(c => !this.isSquareComponent(c))) {
         throw wrongUseError
       } else {
-        return [definedChildren, 'square']
+        return 'square'
       }
     }
   },
@@ -68,7 +76,7 @@ export default {
   },
 
   provide () {
-    let [, childType] = this.children
+    let childType = this.childType
     if (childType === 'map') {
       let $$grid = this._props
       return { $$grid }
@@ -79,7 +87,12 @@ export default {
     let options = this._props
     validateGridOptions(options)
 
-    let [children, childType] = this.children
+    let children = this.$slots.default.filter(c => c.tag !== undefined)
+    let childType = this.childType
+
+    if (childType === undefined) {
+      return createElement('g', { class: 'layout-grid' })
+    }
 
     if (childType === 'square') {
       let squareComponents = children

src/components/Core/utils/grid.js
@@ -112,11 +112,13 @@ export function updateComponent (component, layout) {
 }
 
 function mergeProps (coords, other) {
+  let props = Object.assign({}, coords)
+
   for (let key in other) {
     if (!['x1', 'x2', 'y1', 'y2', 'x', 'y', 'w', 'h'].includes(key)) {
-      coords[key] = other[key]
+      props[key] = other[key]
     }
   }
 
-  return coords
+  return props
 }

stories/sandbox/SimpleGridLayout.vue
@@ -1,40 +1,47 @@
 <template>
-  <vgg-graphic
-    :width="500"
-    :height="500"
-  >
-
-    <vgg-grid
-      :cols="2"
-      :layout-padding="20"
-      :cell-padding="{ l: 7, r: 20, b: 4 }"
+  <div>
+    <vgg-graphic
+      :width="500"
+      :height="500"
     >
 
-      <!-- <vgg-section
-        v-for="s in range(5)"
-        :key="s"
-        :scale-x="[0, 1]"
-        :scale-y="[0, 1]"
+      <vgg-grid
+        :cols="2"
+        :layout-padding="20"
+        :cell-padding="{ l: 7, r: 20, b: 4 }"
       >
 
-        <vgg-rectangle
-          :x1="0"
-          :x2="1"
-          :y1="0"
-          :y2="1"
-          :fill="randomColor()"
+        <!-- <vgg-section
+          v-for="s in range(5)"
+          :key="s"
+          :scale-x="[0, 1]"
+          :scale-y="[0, 1]"
+        >
+
+          <vgg-rectangle
+            :x1="0"
+            :x2="1"
+            :y1="0"
+            :y2="1"
+            :fill="randomColor()"
+          />
+
+        </vgg-section> -->
+        <custom-square-component
+          v-for="s in range(5)"
+          :key="s"
+          :fill="dummy === 'a' ? randomColor() : 'green'"
         />
 
-      </vgg-section> -->
-      <custom-square-component
-        v-for="s in range(5)"
-        :key="s"
-        :fill="randomColor()"
-      />
+      </vgg-grid>
 
-    </vgg-grid>
+    </vgg-graphic>
 
-  </vgg-graphic>
+    <select v-model="dummy">
+      <option value="a">A</option>
+      <option value="b">B</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -42,6 +49,12 @@ import CustomSquareComponent from './CustomSquareComponent.vue'
 
 export default {
   components: { CustomSquareComponent },
+
+  data () {
+    return {
+      dummy: 'a'
+    }
+  },
 
   methods: {
     range (n) {