8bada277aba5221e360762fd231dd1967ba09667
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-04-15 14:17:19 +0200

    refactor: Line exporting renderSVG function

--- src/components/Marks/Line.vue
+++ src/components/Marks/Line.vue
@@ -117,24 +117,75 @@ export default {
     },
 
     renderSVG (createElement) {
-      let aesthetics = this._props
-      let coords = [
-        [aesthetics.x1, aesthetics.y1],
-        [aesthetics.x2, aesthetics.y2]
-      ]
-      let path = this.createPath(aesthetics.func, coords)
-
-      return createElement('path', {
-        attrs: {
-          'd': path
-        },
-        style: createSVGStyle(aesthetics)
-      })
+      return renderSVG(
+        createElement, this.$$transform, this._props,
+        this.$$coordinateTreeParent, this.$$coordinateTree, this._interpolate,
+        this.events, this.addToSpatialIndex
+      )
     },
 
     addToSpatialIndex (coordinates, events) {
       this.$$interactionManager.addItem(this.uuid, 'line', coordinates, this, events, this.sectionParentChain)
     }
   }
+}
+
+export function renderSVG (
+  createElement, $$transform, props,
+  $$coordinateTreeParent, $$coordinateTree, interpolate,
+  events, addToSpatialIndex
+) {
+  let coords = [
+    [props.x1, props.y1],
+    [props.x2, props.y2]
+  ]
+
+  let path = createLinePath(
+    props.func, coords, $$transform, $$coordinateTreeParent, $$coordinateTree,
+    interpolate, events, addToSpatialIndex
+  )
+
+  return createElement('path', {
+    attrs: {
+      'd': path
+    },
+    style: createSVGStyle(props)
+  })
+}
+
+function createLinePath (
+  func, coords, $$transform, $$coordinateTreeParent, $$coordinateTree,
+  interpolate, events, addToSpatialIndex
+) {
+  let transformedPoints
+  let path
+
+  if (func) {
+    let parentId = $$coordinateTreeParent
+    let domains = $$coordinateTree.getBranch(parentId).domains
+
+    let points = interpolatePointsFromFunc(func, domains)
+    transformedPoints = transformPoints(points, $$transform)
+    path = createPath(transformedPoints)
+  }
+
+  if (!func) {
+    if (interpolate) {
+      let points = interpolatePoints(coords)
+      transformedPoints = transformPoints(points, $$transform)
+      path = createPath(transformedPoints)
+    }
+
+    if (!interpolate) {
+      transformedPoints = transformPoints(coords, $$transform)
+      path = createPath(transformedPoints)
+    }
+  }
+
+  if (events.length > 0) {
+    addToSpatialIndex(transformedPoints, events)
+  }
+
+  return path
 }
 </script>
