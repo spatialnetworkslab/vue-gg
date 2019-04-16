64048c9bad5fcc30a14d9cce5cc0d9e99c85ed7b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-04-16 16:59:45 +0200

    feat: started on BatchRenderer

---
+++ src/components/Core/BatchRenderer.vue
@@ -0,0 +1,25 @@
+<script>
+import { renderSVG as renderLabelSVG } from '../../rendering/label.js'
+import { renderSVG as renderLineSVG } from '../../rendering/line.js'
+import { renderSVG as renderPathSVG } from '../../rendering/path.js'
+import { renderSVG as renderPointSVG } from '../../rendering/point.js'
+import { renderSVG as renderRectangleSVG } from '../../rendering/rectangle.js'
+import { renderSVG as renderSymbolSVG } from '../../rendering/symbol.js'
+import { renderSVG as renderTrailSVG } from '../../rendering/trial.js'
+
+export default {
+  props: {
+    content: {
+      type: Array,
+      required: true
+    }
+  },
+
+  render (createElement) {
+    let elements = []
+    for (let elementOptions of this.content) {
+      
+    }
+  }
+}
+</script>
