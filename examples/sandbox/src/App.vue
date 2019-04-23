086d2b035d9dfab018197fb33a2413fb8f44f64a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2019-04-23 16:04:25 +0200

    fix: global scale with scale-x/y props + temporal nice scales
    * fix: coercion of Date to Number in 'nice' scale logic
    
    * fix: global scale with scale-x/scale-y props with no loaded data
    
    * fix: sandbox working as expected now!

--- examples/sandbox/src/App.vue
+++ examples/sandbox/src/App.vue
@@ -1,71 +1,134 @@
 <template>
-  <vgg-graphic
-    :width="500"
-    :height="500"
-    :data="trains"
-  >
-
-    <vgg-scales :scales="{
-        stationScale: 'station',
-        timeScale: 'time',
-        trainScale: 'train'
-      }"
-    />
-
-
-    <vgg-section
-      :x1="50"
-      :x2="450"
-      :y1="50"
-      :y2="450"
-      :transform="{ groupBy: 'train' }"
+  <div>
+    <vgg-graphic
+      :width="1500"
+      :height="600"
+      :data="train_timetable"
     >
 
-      <vgg-map v-slot="{ row }">
+      <!-- <vgg-scales :scales="{
+        timeScale: { domain: [new Date('April 22, 2019 00:04:00'), new Date('April 23, 2019 00:04:00')] },
+        stationScale: { domain: 'station' }
+      }"
+      /> -->
+      <vgg-scales :scales="{
+        timeScale: { domain: [new Date('April 22, 2019 04:00:00'), new Date('April 23, 2019 04:00:00')] },
+        stationScale: { domain: 'station' }
+      }"
+      />
 
-        <vgg-multi-line
-          :x="{ val: row.grouped.time, scale: '#timeScale' }"
-          :y="{ val: row.grouped.station, scale: '#stationScale' }"
-          :stroke="{ val: row.train, scale: '#trainScale' }"
-          @hover="log(row)"
+      <vgg-section
+        :x1="150"
+        :x2="1450"
+        :y1="50"
+        :y2="550"
+        :transform="{ groupBy: 'train' }"
+      >
+
+        <vgg-map v-slot="{ row }">
+
+          <vgg-multi-line
+            :x="{val: row.grouped.time, scale: '#timeScale'}"
+            :y="{val: row.grouped.station, scale: '#stationScale'}"
+            :stroke="'#009900'"
+          />
+
+        </vgg-map>
+
+        <vgg-x-axis
+          :scale="'#timeScale'"
+          :tick-count="24"
+          :tick-extra="false"
+          :format="(date) => date.toLocaleTimeString([], {hour12: false, hour: '2-digit', minute: '2-digit'})"
         />
 
-      </vgg-map>
+        <vgg-y-axis
+          :scale="'#stationScale'"
+          :format="(station) => station.split('.').pop()"
+        />
 
-    </vgg-section>
+        <vgg-x-grid
+          :scale="[new Date('April 22, 2019 00:04:00'), new Date('April 23, 2019 00:04:00')]"
+          :grid-line-count="48" :grid-colour="'white'"
+        />
 
-  </vgg-graphic>
+        <vgg-y-grid
+          :scale="[0,19]"
+          :grid-line-count="20"
+        />
+
+      </vgg-section>
+
+    </vgg-graphic>
+
+    <!-- <div class="line-selector">
+      <label>Select Line</label>
+      <multiselect v-model="selectedLine"
+        :options="availableLines"
+        :allow-empty="false"
+        :custom-label="prettify_linename"/>
+      <label>{{selectedLine}}</label>
+    </div> -->
+
+  </div>
+
 </template>
 
 <script>
-/* eslint-disable */
-import trainTimetable from './odpt_TrainTimetable_edited.json'
+import train_timetable from './odpt_TrainTimetable.json'
+// import stations from './odpt_Station'
+
+import Multiselect from 'vue-multiselect'
 
 export default {
   name: 'app',
-
+  components: { Multiselect },
+  data () {
+    return {
+      trains: train_timetable,
+      // stations: Object.freeze(stations),
+      availableLines: ['odpt.Railway:TokyoMetro.Chiyoda'],
+      selectedLine: 'odpt.Railway:TokyoMetro.Chiyoda',
+      hoveredRow: {},
+      selectedRow: {}
+    }
+  },
+  methods: {
+    prettify_linename (railway) {
+      return `${railway.split('.').pop()} Line`
+    },
+    onHover (row) {
+      this.hoveredRow = row
+    },
+    onSelect (row) {
+      this.hoveredRow = {}
+      this.selectedRow = row === this.selectedRow ? {} : row
+    }
+  },
   computed: {
-    trains () {
-      let allStops = {
+    train_timetable () {
+      // Thanks to Luuc for providing this
+      let timetable = {
         train: [],
         station: [],
         time: []
       }
 
-      for (let trainData of trainTimetable) {
+      for (let trainData of this.trains) {
         let train = trainData.train
-        let stopsForThisTrain = trainData.trainTimetableObject
+        let stops = trainData.trainTimetableObject
 
-        for (let stop of stopsForThisTrain) {
-          allStops.train.push(train)
-          allStops.station.push(stop.Station)
+        for (let stop of stops) {
+          timetable.train.push(train)
+          timetable.station.push(stop.Station)
 
           let date = new Date(`April 22, 2019 ${stop.Time}:00`)
-          allStops.time.push(date)
+
+          timetable.time.push(date)
         }
       }
-
-      return allStops
+      // console.log(timetable)
+      return timetable
     }
   },
 
@@ -74,3 +137,15 @@ export default {
   }
 }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style>
+.line-selector {
+  width: 300px;
+  float: left;
+}
+.line-diagram {
+  float: left;
+}
+</style>
+
